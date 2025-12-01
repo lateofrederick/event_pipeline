7b2f7516607bdc98b35ee54ac3f31195832b2863
nshaibu <nafiushaibu1@gmail.com>
2025-12-01 17:45:14 +0000

    modify github action scirpts

--- volnux/typing.py
+++ volnux/typing.py
@@ -8,10 +8,14 @@ from typing import (
     Iterator,
     List,
     Optional,
-    TypeAlias,
     TypeVar,
     Union,
 )
+
+try:
+    from typing import TypeAlias
+except ImportError:
+    from typing_extensions import TypeAlias
 
 
 class ConfigState(Enum):
