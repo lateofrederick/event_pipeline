f7213caf7ea80f6f8ea1beb46544ea1eba03aaf1
nshaibu <nafiushaibu1@gmail.com>
2026-04-17 02:48:34 +0000

    refactor the entire project

---
+++ volnux/cli/command/builtins/manifest/__init__.py
@@ -0,0 +1,8 @@
+"""
+Two CLI commands for EventHub manifest management:
+
+    volnux validate_manifest [--manifest PATH]
+    volnux generate_manifest [--output PATH] [--package-name NAME]
+                             [--package-version VERSION] [--source-type TYPE]
+                             [--dry-run]
+"""
